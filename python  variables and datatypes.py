a742b2c85d75576fe57f16867e8a0d81a4073139
Jyotiraditya <jyotiraditya20122004@gmail.com>
2026-03-14 16:46:14 +0530

    init

---
+++ python  variables and datatypes.py
@@ -0,0 +1,255 @@
+#datatypes are-
+#int
+#bool
+#string
+#float
+a=5
+print(type(a))
+b="5","jyotir"
+print(type(b))
+c=9.5
+print(type(c))
+d=True,False
+print(type(d))
+
+
+#variables
+#a,b,c,d-variables
+
+#operators
+#arithmatic- +, -, *, /, %, **, //
+#relational/comparisonal- <, >, >=, <=, ==, !=
+#assighnment operator- ==,!=,+=, -=, *=, /=, **=
+# logical operator- and, or, not
+
+
+a=10
+b=20
+c=a+b
+print(c)
+
+a=100
+b=70
+print(a+b)
+
+#substract
+a=200
+b=60
+c=a-b
+print(c)
+
+a=40
+b=20
+print(a-b)
+
+#Multiply
+a=80
+b=7
+c=80*9
+print(c)
+
+a=12
+b=2
+print(a*b)
+
+
+#division
+a=230
+b=2
+c=a/b
+print(c)
+
+a=120
+b=4
+print(a/b)
+
+#modulous
+a=70
+b=3
+c=a%b
+print(c)
+
+
+a=90
+b=7
+print(a%b)
+
+#power
+a=60
+b=2
+c=a**b
+print(c)
+
+
+a=70
+b=3
+print(a**b)
+
+
+#flor division
+a=40
+b=5
+c=a//b
+print(c)
+
+
+
+a=90
+b=3
+print(a//b)
+
+#relational operator
+a=100
+b=10
+print(a==b)
+print(a!=b)
+ 
+a=80
+b=10
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#assighnment operators
+a=10
+a+=10
+print(a)
+
+a=20
+a=a+80
+print(a)
+
+b=40
+b+=20
+print(b)
+
+b=50
+b=b+90
+print(b)
+
+c=70
+c-=60
+print(c)
+
+
+c=80
+c=c-30
+print(c)
+
+d=90
+d-=80
+print(d)
+
+e=250
+e=e-50
+print(e)
+
+f=23
+f=f*4
+print(f)
+
+f=48
+f*=12
+print(f)
+
+g=60
+g=g/10
+print(g)
+
+h=120
+h/=12
+print(h)
+
+i=470
+i=i**3
+print(i)
+
+i=500
+i**=5
+print(i)
+
+g=800
+g=g%9
+print(g)
+
+h=400
+h=h//3
+print(h)
+
+i=800
+i//=30
+print(i)
+
+#logical oprators
+a=89
+b=23
+print(not(a>b))
+print(not(a<b))
+a=True
+print(not(a))
+
+a=78
+b=34
+print(a!=b and a>b)
+print(a==b and a>b)
+print(a!=b and a>b)
+print(a==b and a<=b)
+
+
+
+a=89
+b=16
+print(a!=b or a>b)
+print(a==b or a>b)
+print(a>=b or a>b)
+
+a=90
+b=40
+print(a>b or a==b)
+print(a!=b or a<b)
+print(a<=b or a!=b)
+print(a!=b or a<=b)
+print(a!=b or a<=b)
+
+
+#Type casting
+a=70
+b=int("90")
+print(a+b)
+
+a="jyotir"
+b="aditya"
+print(a+b)
+
+f_name=input("enter the first name= ")
+l_name=input("enter the last name= ")
+full_name=input(f_name+" "+l_name)
+age=int(input("enter the age="))
+marks=int(input("enter the number="))
+print(f_name)
+print(l_name)
+print(full_name)
+print(age)
+print(marks)
+print(f_name,l_name,full_name,age,marks)
+
+
+
+
+a=float(input("enter the side="))
+print("area",a**2)
+
+a=int(input("enter the number="))
+b=int(input("enter the number="))
+c=int(input("enter the number="))
+print("average",a+b+c/3)
+
+a=int(input("enter the number="))
+b=int(input("enter the number="))
+print(a>b)
+
+
+
+
+
